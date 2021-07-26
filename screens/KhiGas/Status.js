a586386f467a17bec624a924d25c382e47b4f5cf
phuocthuan14 <phuocthuan1412@gmail.com>
2021-07-26 23:54:36 +0700

    cong tac khi gas

--- screens/KhiGas/Status.js
+++ screens/KhiGas/Status.js
@@ -1,7 +1,7 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
-import BlinkView from 'react-native-blink-view'
+import { db } from "../../Components/FirebaseConfig";
 
 export default function Status() {
   const unactive = require("../../assets/icon/lock_red.png")
@@ -9,17 +9,52 @@ export default function Status() {
   const both = { unactive, active }
   const [image, setImage] = useState(unactive)
   const [isActive, setIsActive] = useState(false)
+
+  const getData = () => {
+    db.ref("/request/gas_sensor")
+      .once('value', snapshot => {
+        //setUser({name:snapshot.val().name, email: snapshot.val().email})
+        //console.log(snapshot.val() )
+        setIsActive(Boolean(snapshot.val().status));
+        if (isActive) {
+          setImage(active)
+        }
+        else {
+          setImage(unactive)
+        }
+        //setLoading(true);
+      });
+  }
+  const swipeUp = () => {
+    setImage(active)
+    setIsActive(true)
+    db.ref("/request/gas_sensor").set({
+      status: 1,
+      value: "ON"
+    }).then((snapshot) => {
+      console.log('GAS DETECT BUTTON SWITCH ON')
+    })
+  }
+
+  const swipeDown = () => {
+    setImage(unactive)
+    setIsActive(false)
+    db.ref("/request/gas_sensor").set({
+      status: 0,
+      value: "OFF"
+    }).then(
+      console.log("GAS DETECT BUTTON SWITCH OFF")
+    )
+  }
+  
+  useEffect(() => {
+    getData()
+  }, [isActive]);
   return (
     <GestureRecognizer
       style={{ flex: 1 }}
-      onSwipeUp={() => {
-        setImage(active)
-        setIsActive(true)
-      }}
-      onSwipeDown={() => {
-        setImage(unactive)
-        setIsActive(false)
-      }}
+      onSwipeUp={swipeUp}
+      onSwipeDown={swipeDown}
     >
       <View style={[styles.container]}>
         {isActive ? null : <Text>Kích hoạt</Text>}
@@ -32,7 +67,7 @@ export default function Status() {
         {isActive ? <Text>Tắt</Text> : null}
 
       </View>
-    </GestureRecognizer>
+    </GestureRecognizer >
   );
 }
 
